b01e486589f2990ff397ec34ba8b86ddded8e362
Imre Bartis <imre.bartis@siili.com>
2018-01-09 16:19:27 +0200

    watcher responding to async updates

--- script.js
+++ script.js
@@ -2,43 +2,31 @@ var card = new Vue({
     el: "#card",
     data: {
       title: "Dinosaurs",
-      input:"",
-      speciesUpdated: 0,
-      dinosUpdated: 0,
+      input: "",
+      buttonText: "Add Dinosaur",
       items: [
-        { text: "Tyrannosaurus", quantity: 5 },
-        { text: "Triceratops", quantity: 3 },
-        { text: "Stegosaurus", quantity: 6 }
+        "Tyrannosaurus",
+        "Triceratops",
+        "Stegosaurus"
       ]
     },
     computed: {
-      totalDinos: function(){
-        this.dinosUpdated += 1;
-        var sum = 0;
-        var items = this.items;
-        
-        for(var i in items) {
-          sum += items[i].quantity;
-        }
-        
-        return sum;
-      },
-      totalSpecies: function(){
-        this.speciesUpdated += 1;
-        return this.items.length;
+      buttonDisabled: function() {
+        return this.input == "";
       }
     },
+    watch: {
+      input: _.debounce(function(){
+        this.buttonText = this.input !== "" ? "Add " + this.input : "Add Dinosaur";
+      }, 250)
+    },
     methods: {
-      addItem: function (e){
-        e.preventDefault();
-        if (!this.input) return;
-        
-        this.items.push({text: this.input, quantity: 1});
-        this.input = "";
-      },
-      removeItem: function (item){
-        this.items.splice(item, 1);
+      addItem: function (){
+        if (this.input !== '') {
+          this.items.push( this.input );
+          
+          this.input = '';
+        }
       }
     }
   });
-  
